74a0cf39977c8f1a1e7b82ab6d0eedd6c1f30b6b
acerroblanco <acerroblanco@gmail.com>
2018-11-06 14:41:52 -0800

    Completed method for db access

--- AAquestions/db_classes.rb
+++ AAquestions/db_classes.rb
@@ -16,6 +16,10 @@ class Users
 
   attr_accessor :id, :fname, :lname
 
+  def self.authored_replies(id)
+    Reply.find_by_user_id(id)
+  end
+
   def self.find_by_id(id)
     data = QuestionsDatabase.instance.execute(<<-SQL, id)
       SELECT *
@@ -40,17 +44,26 @@ class Users
     @lname = options_hash['lname']
   end
 
+  def authored_questions
+    data = QuestionsDatabase.instance.execute(<<-SQL, @id)
+      SELECT *
+      FROM questions
+      WHERE author_id = ?;
+    SQL
+    data.map { |e| Questions.new(e) }
+  end
+
 end
 
 class Questions
 
   attr_accessor :id, :title, :body, :author_id
 
-  def self.find_by_id(id)
-    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+  def self.find_by_author_id(author_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, author_id)
       SELECT *
       FROM questions
-      WHERE id = ?;
+      WHERE author_id = ?;
     SQL
     data.map { |e| Questions.new(e) }
   end
@@ -61,6 +74,46 @@ class Questions
     @body = options_hash['body']
     @author_id = options_hash['author_id']
   end
+
+  def author
+    data = QuestionsDatabase.instance.execute(<<-SQL, @author_id)
+      SELECT *
+      FROM questions
+      WHERE author_id = ?;
+    SQL
+    data.map { |e| Questions.new(e) }
+  end
+end
+
+class Replies
+
+  def self.find_by_user_id(user_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT *
+      FROM replies
+      WHERE user_id = ?;
+    SQL
+    data.map { |e| Replies.new(e) }
+  end
+
+  def self.find_by_question_id(question_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT *
+      FROM replies
+      WHERE question_id = ?;
+    SQL
+    data.map { |e| Replies.new(e) }
+  end
+
+  def initialize(options_hash)
+    @id = options_hash['id']
+    @subject_question_id = options_hash['subject_question_id']
+    @subject_question_title = options_hash['subject_question_title']
+    @question = options_hash['question']
+    @parent_reply = options_hash['parent_reply']
+    @reply_author_id = options_hash['reply_author_id']
+  end
+
 end
 
 class QuestionFollows
@@ -69,9 +122,5 @@ class QuestionFollows
 end
 
 class QuestionLikes
-
-end
-
-class Replies
 
 end
