1ab48086fdb35e17d55b8d1bf5d179d20cfbfde5
acerroblanco <acerroblanco@gmail.com>
2018-11-06 13:37:13 -0800

    Finish creating db

--- AAquestions/import_db.sql
+++ AAquestions/import_db.sql
@@ -13,43 +13,55 @@ CREATE TABLE users (
 );
 
 CREATE TABLE questions (
-  id INTEGER PRIMARY KEY
-  title VARCHAR(255) NOT NULL
-  body TEXT NOT NULL
-  author_id INTEGER NOT NULL
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
 
-  FOREIGN KEY (author_id
+  FOREIGN KEY (author_id)
     REFERENCES users(id)
-  );
 );
 
 CREATE TABLE question_follows (
-  user_id INTEGER PRIMARY KEY
-  question_id INTEGER
+  user_id INTEGER PRIMARY KEY,
+  question_id INTEGER,
 
-  FOREIGN KEY (question_id
-    REFERENCES questions(id)
-  );
-
+  FOREIGN KEY (question_id)
+    REFERENCES questons(id)
 );
 
 CREATE TABLE replies (
-  id INTEGER PRIMARY KEY
-  subject_question_id INTEGER NOT NULL
-  subject_question_title VARCHAR(255) NOT NULL
-  question TEXT
-  parent_reply INTEGER
-  reply_author_id INTEGER NOT NULL
+  id INTEGER PRIMARY KEY,
+  subject_question_id INTEGER NOT NULL,
+  subject_question_title VARCHAR(255) NOT NULL,
+  question TEXT,
+  parent_reply INTEGER,
+  reply_author_id INTEGER NOT NULL,
 
-  FOREIGN KEY (parent_reply
+  FOREIGN KEY (parent_reply)
     REFERENCES replies(id)
-  );
 );
 
 CREATE TABLE questions_likes (
-  user_id INTEGER PRIMARY KEY
-  question_id INTEGER
+  user_id INTEGER PRIMARY KEY,
+  question_id INTEGER,
 
-  FOREIGN KEY (question_id
-    REFERENCES questions(id))
+  FOREIGN KEY (question_id)
+    REFERENCES questions(id)
 );
+
+
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Armando', 'C'),
+  ('Theo', 'Obbard'),
+  ('Ryan', 'Mapa');
+
+  INSERT INTO
+    questions (title, body, author_id)
+  VALUES
+    ('HELP W JOINS', 'I need help with joins', 1),
+    ('Help w life', 'eeeerg help meh', 2),
+    ('Am I cool?', 'need an answer asap', 3);
